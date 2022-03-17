3b358bda322fd3a57e699ae42a441a81276c8831
Jaime Roncal <55926526+jaimeroncal@users.noreply.github.com>
2022-03-17 21:31:48 +0100

    Versión 0 Android Studio
    Primera pantalla inicio de credenciales y algunas fuentes y colores.

---
+++ Frankenstory/app/src/main/java/es/unizar/eina/frankenstory/MainActivity.java
@@ -0,0 +1,14 @@
+package es.unizar.eina.frankenstory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
