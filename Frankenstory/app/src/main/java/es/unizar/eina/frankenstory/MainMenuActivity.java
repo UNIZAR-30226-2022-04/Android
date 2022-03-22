a6353bbbccc317f2e5011a5dea521b865fdaaea5
Jaime Roncal <55926526+jaimeroncal@users.noreply.github.com>
2022-03-22 14:20:52 +0100

    Modificaciones varias
    Titulo a foto, títulos menú abajo, ejecuta bien en el móvil.

--- Frankenstory/app/src/main/java/es/unizar/eina/frankenstory/MainMenuActivity.java
+++ Frankenstory/app/src/main/java/es/unizar/eina/frankenstory/MainMenuActivity.java
@@ -2,9 +2,14 @@ package es.unizar.eina.frankenstory;
 
 import androidx.appcompat.app.AppCompatActivity;
         import android.content.Intent;
-        import android.os.Bundle;
+import android.database.Cursor;
+import android.os.Bundle;
         import android.view.View;
-        import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -12,7 +17,6 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-
     }
 
     // Para ocultar Navigation bar y lo de arriba.
