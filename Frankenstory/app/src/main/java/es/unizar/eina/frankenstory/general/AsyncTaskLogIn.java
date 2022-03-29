1a07e24da52bd2d92ac4dbcc78742460327d38d7
Jaime Roncal <55926526+jaimeroncal@users.noreply.github.com>
2022-03-29 13:45:11 +0200

    Conexión LogIn principalemente
    Además cambio de visionado de modo en activity_quick_game_room, y cambio de logo de app (no el de MoonCode).

---
+++ Frankenstory/app/src/main/java/es/unizar/eina/frankenstory/general/AsyncTaskLogIn.java
@@ -0,0 +1,63 @@
+package es.unizar.eina.frankenstory.general;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AsyncTaskLogIn extends AsyncTask<String, Void, AsyncTaskLogIn.Result> {
+
+    private LogInActivity mActivity = null;
+
+    static class Result {
+        String result;
+        String reason;
+    }
+
+    public AsyncTaskLogIn(LogInActivity activity)
+    {
+        mActivity = activity;
+    }
+
+    protected Result doInBackground(String... params) {
+        String username = params[0];
+        String password = params[1];
+        HttpURLConnection con;
+        try {
+            con = (HttpURLConnection) new URL("https://mooncode-frankenstory-dev.herokuapp.com/api/login").openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+
+            String jsonInputString = "{\"username\":\""+username+"\",\"password\":\""+password+"\"}";
+            try(OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes();
+                os.write(input, 0, input.length);
+            }
+
+            InputStreamReader reader = new InputStreamReader(con.getInputStream());
+            Gson gson = new Gson();
+            return gson.fromJson(reader, Result.class);
+
+        } catch (IOException e) {
+            Log.e("ERROR_AsyncTaskLogin",e.getMessage());
+        }
+        return new Result();
+    }
+
+    protected void onPostExecute(Result resultado) {
+        if (resultado.result.equals("success")) {
+            mActivity.setupAdapter();
+        } else {
+            Toast.makeText(mActivity, "ERROR:"+resultado.reason, Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- Frankenstory/app/src/main/java/es/unizar/eina/frankenstory/general/LogInActivity.java
+++ Frankenstory/app/src/main/java/es/unizar/eina/frankenstory/general/LogInActivity.java
@@ -5,21 +5,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import es.unizar.eina.frankenstory.R;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private EditText mUserName;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_log_in);
 
+        mUserName = (EditText) findViewById(R.id.usernameLogIn);
+        mPassword = (EditText) findViewById(R.id.passwordLogIn);
+
         Button button = (Button)findViewById(R.id.login);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent i = new Intent(LogInActivity.this, MainMenuActivity.class);
-                startActivity(i);
+                // LEER EDIT TEXTS
+                String username = mUserName.getText().toString();
+                String password = mPassword.getText().toString();
+                boolean camposLlenos = true;
+                if (username.equalsIgnoreCase("")){
+                    mUserName.setError("Introduce un nombre de usuario");
+                    camposLlenos = false;
+                }
+                if (password.equalsIgnoreCase("")){
+                    mPassword.setError("Introduce una contraseña");
+                    camposLlenos = false;
+                }
+
+                // LLAMAR A LA TAREA ASINCRONA
+                if (camposLlenos){
+                    AsyncTaskLogIn myTask = new AsyncTaskLogIn(LogInActivity.this);
+                    myTask.execute(username, password);
+                    Toast.makeText(LogInActivity.this, "Preguntando a la web", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
@@ -40,6 +65,13 @@ public class LogInActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             );
         }
+    }
+
+    // ASYNC TASK ADAPTER
+    public void setupAdapter()
+    {
+        Intent i = new Intent(LogInActivity.this, MainMenuActivity.class);
+        startActivity(i);
     }
 
 }
